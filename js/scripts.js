9e57e1fbe1375d1657fe2704b7cc0bfc0aa5b1fb
burdas <burdasparmarcos@gmail.com>
2020-10-18 20:32:57 +0200

    Añadido cambios visuales, configuración, gráfico e info

--- js/scripts.js
+++ js/scripts.js
@@ -1,73 +1,263 @@
+/* Valores por defecto para la distribución de macros
+se pueden cambiar en la configuración*/
+const defaultPGanancia = 2;
+const defaultPMantenimiento = 2;
+const defaultPPerdida = 2.5;
+const defaultGHombre = 0.8;
+const defaultGMujer = 1.2;
+const defaultOCalorias = 300;
+
+/* Valores de la distribución de macros seleccionada, por defecto los de arriba */
+var pGanancia, pMantenimiento, pPerdida, gHombre, gMujer, oCalorias;
+
+/* Controladores de los ranger slider para seleccionar la distribución de macros
+del apartado de configuración*/
+var rangePGanancia, rangePMantenimiento, rangePPerdida, rangeGHombre, rangeGMujer, rangeOCalorias;
+
+/** Variable del grafico tipo donut */
+var donut;
 $(document).ready(function () {
-    $('#formulario').submit(function (e) {
+    inicializarRanges();
+    inicializarParametros();
+
+    resetConfig();
+
+    // Botón de restablecer, devuleve en el aprtado de configuracón
+    // a los valores, por defecto
+    $('#btnRestablecer').click(function (e) {
         e.preventDefault();
-        var grProteina, calProteina, grGrasas, calGrasas, grHidratos, calHidratos;
-        var genero = $("input[name='genero']:checked").val();
-        var peso = parseInt($("#pesoKg").val());
-        var estatura = parseInt($("#estaturaCm").val());
-        var edad = parseInt($("#edad").val());
-        var ejercicio = $("input[name='ejercicio']:checked").val();
-        var objetivo = $("input[name='objetivo']:checked").val();
-        console.log("Genero: " + genero + " Peso: " + peso + " Estatura: " + estatura + " Edad: " + edad + " Ejercicio: " + ejercicio + " Objetivo: " + objetivo);
-        var tmb = calHarrisBennedict(peso, estatura, edad, genero);
-        var recomendada = ingestaRecomendada(tmb, ejercicio);
-        var difereciaCal;
-        switch (objetivo) {
-            case "1":
-                difereciaCal = -300;
-                grProteina = 2.5 * peso;
-                break;
-            case "3":
-                difereciaCal = 300;
-                grProteina = 2 * peso;
-                break;
-            default:
-                difereciaCal = 0;
-                grProteina = 2 * peso;
-                break;
-        }
-        var caloriasObjetivo = recomendada + difereciaCal;
-        $('#tmb').text(Math.round(tmb) + " Cal");
-        $('#recomendada').text(Math.round(recomendada) + " Cal");
-        $('#calObjetivo').text(Math.round(caloriasObjetivo) + " Cal");
-        calProteina = 4 * grProteina;
-        if (genero == "mujer") {
-            grGrasas = 1.2 * peso;
-        } else {
-            grGrasas = 0.8 * peso;
-        }
-        calGrasas = grGrasas * 9;
-        calHidratos = caloriasObjetivo - calGrasas - calProteina;
-        grHidratos = calHidratos/4;
-        $('#hidratos').text(Math.round(calHidratos) + " Cal / " + Math.round(grHidratos) + " gr");
-        $("#pbHidratos")
-            .attr("aria-valuemax", caloriasObjetivo)
-            .css("width", Math.round(calHidratos/caloriasObjetivo*100) + "%")
-            .attr("aria-valuenow", calHidratos);
-        $('#grasas').text(Math.round(calGrasas) + " Cal / " + Math.round(grGrasas) + " gr");
-        $("#pbGrasas")
-            .attr("aria-valuemax", caloriasObjetivo)
-            .css("width", Math.round(calGrasas/caloriasObjetivo*100) + "%")
-            .attr("aria-valuenow", calGrasas);
-        $('#proteinas').text(Math.round(calProteina) + " Cal / " + Math.round(grProteina) + " gr");
-        $("#pbProteinas")
-            .attr("aria-valuemax", caloriasObjetivo)
-            .css("width", Math.round(calProteina/caloriasObjetivo*100) + "%")
-            .attr("aria-valuenow", calProteina);
-        return false;
+        resetConfig();
     });
+    $('#btnVolverCalculadora').click(function (e) { 
+        e.preventDefault();
+        $("#formulario").toggle();
+        $("#resultados").toggle();
+        $('#btnVolverCalculadora').toggle();
+    });
+
+    $(window).on('resize', resizeManager);
+
+    $('#formulario').submit(calcular);
+    $('#formConfig').submit(guardarConfiguracion);
 });
 
+// Guarda los valores de distribución de macros seleccionada
+// en el apratado de configuración en las variables
+function guardarConfiguracion(e) {
+    e.preventDefault();
+    pGanancia = rangePGanancia.value;
+    pMantenimiento = rangePMantenimiento.value;
+    pPerdida = rangePPerdida.value;
+    gHombre = rangeGHombre.value;
+    gMujer = rangeGMujer.value;
+    oCalorias = rangeOCalorias.value;
+    console.log('gr proteina por kilo de peso - Ganancia: ' + pGanancia);
+    console.log('gr proteina por kilo de peso - Mantenimiento: ' + pMantenimiento);
+    console.log('gr proteina por kilo de peso - Perdida: ' + pPerdida);
+    console.log('gr grasa por kilo de peso - Hombre: ' + gHombre);
+    console.log('gr grasa por kilo de peso - Mujeres: ' + gMujer);
+    console.log('Calorias para el objetivo: ' + oCalorias);
+    $('#modalConfig').modal('toggle');
+    return false;
+}
+
+/** Inicializa las variables de distribución de macros a
+ * los valores por defecto
+*/
+function inicializarParametros() {
+    pGanancia = defaultPGanancia;
+    pMantenimiento = defaultPMantenimiento;
+    pPerdida = defaultPPerdida;
+    gHombre = defaultGHombre;
+    gMujer = defaultGMujer;
+    oCalorias = defaultOCalorias;
+}
+
+/**Obtenemos los range input del DOM
+ * Despues les asignamos el manejador que muestra
+ * en texto los valores del dom
+ */
+function inicializarRanges() {
+    rangePGanancia = document.getElementById('rangePGanancia');
+    rangePMantenimiento = document.getElementById('rangePMantenimiento');
+    rangePPerdida = document.getElementById('rangePPerdida');
+    rangeGHombre = document.getElementById('rangeGHombre');
+    rangeGMujer = document.getElementById('rangeGMujer');
+    rangeOCalorias = document.getElementById('rangeOCalorias');
+
+    rangePGanancia.addEventListener("input", cambiarVistaRange);
+    rangePMantenimiento.addEventListener("input", cambiarVistaRange);
+    rangePPerdida.addEventListener("input", cambiarVistaRange);
+    rangeGHombre.addEventListener("input", cambiarVistaRange);
+    rangeGMujer.addEventListener("input", cambiarVistaRange);
+    rangeOCalorias.addEventListener("input", cambiarVistaRange);
+}
+
+/** Muestra los valores del range en texto */
+function cambiarVistaRange() {
+    let name = this.getAttribute('id');
+    document.getElementById(name + 'Vista').innerHTML = this.value;
+}
+
+/** Establece los valores por defecto
+ * de los rangers y el texto
+ */
+function resetConfig() {
+    rangePGanancia.value = defaultPGanancia;
+    rangePMantenimiento.value = defaultPMantenimiento;
+    rangePPerdida.value = defaultPPerdida;
+    rangeGHombre.value = defaultGHombre;
+    rangeGMujer.value = defaultGMujer;
+    rangeOCalorias.value = defaultOCalorias;
+
+    document.getElementById('rangePGanancia' + 'Vista').innerHTML = defaultPGanancia;
+    document.getElementById('rangePMantenimiento' + 'Vista').innerHTML = defaultPMantenimiento;
+    document.getElementById('rangePPerdida' + 'Vista').innerHTML = defaultPPerdida;
+    document.getElementById('rangeGHombre' + 'Vista').innerHTML = defaultGHombre;
+    document.getElementById('rangeGMujer' + 'Vista').innerHTML = defaultGMujer;
+    document.getElementById('rangeOCalorias' + 'Vista').innerHTML = defaultOCalorias;
+}
+
+/** Maneja el formulario de calcular las calorias y macros */
+function calcular(e) {
+    e.preventDefault();
+
+    var genero = $("input[name='genero']:checked").val();
+    var peso = parseInt($("#pesoKg").val());
+    var estatura = parseInt($("#estaturaCm").val());
+    var edad = parseInt($("#edad").val());
+    var ejercicio = $("input[name='ejercicio']:checked").val();
+    var objetivo = $("input[name='objetivo']:checked").val();
+
+    console.log("Genero: " + genero + " Peso: " + peso + " Estatura: " + estatura + " Edad: " + edad + " Ejercicio: " + ejercicio + " Objetivo: " + objetivo);
+
+    $("#formulario").toggle();
+    $('#btnVolverCalculadora').toggle();
+    lanzarSpinner(peso, estatura, edad, genero, ejercicio, objetivo);
+
+    return false;
+}
+
+/** Calcula los gr y calorias y los muestra en el DOM */
+function generarCal(peso, estatura, edad, genero, ejercicio, objetivo) {
+    $("#resultados").toggle();
+    var grProteina, calProteina, grGrasas, calGrasas, grHidratos, calHidratos;
+    var tmb = calHarrisBennedict(peso, estatura, edad, genero);
+    var recomendada = ingestaRecomendada(tmb, ejercicio);
+    var difereciaCal;
+    switch (objetivo) {
+        case "1":
+            difereciaCal = -1 * oCalorias;
+            grProteina = peso * pGanancia;
+            break;
+        case "3":
+            difereciaCal = oCalorias;
+            grProteina = peso * pMantenimiento;
+            break;
+        default:
+            difereciaCal = 0;
+            grProteina = peso * pPerdida;
+            break;
+    }
+    var caloriasObjetivo = recomendada + difereciaCal;
+    animateValue('tmb', Math.round(tmb), " Cal");
+    animateValue('recomendada', Math.round(recomendada), " Cal");
+    animateValue('calObjetivo', Math.round(caloriasObjetivo), " Cal");
+    calProteina = 4 * grProteina;
+    if (genero == "mujer") {
+        grGrasas = peso * gMujer;
+    } else {
+        grGrasas = peso * gHombre;
+    }
+    calGrasas = grGrasas * 9;
+    calHidratos = caloriasObjetivo - calGrasas - calProteina;
+    grHidratos = calHidratos / 4;
+    animateValue('grProteinas', Math.round(grProteina), " gr");
+    animateValue('calProteinas', Math.round(calProteina), " Cal");
+    animateValue('grGrasas', Math.round(grGrasas), " gr");
+    animateValue('calGrasas', Math.round(calGrasas), " Cal");
+    animateValue('grHidratos', Math.round(grHidratos), " gr");
+    animateValue('calHidratos', Math.round(calHidratos), " Cal");
+
+    graficoDonut(grProteina, grGrasas, grHidratos);
+}
+
+/** Muestra el spinner por pantalla
+ * al acabar lanza generaCal()
+ */
+function lanzarSpinner(peso, estatura, edad, genero, ejercicio, objetivo){
+    $('#modalSpinner').modal('toggle');
+    new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve("anything");
+        }, 2000);
+    }).then(function (result) {
+        $('#modalSpinner').modal('toggle');
+        $("html, body").animate({ scrollTop: 2 * $(document).outerHeight(true) }, 2000);
+        generarCal(peso, estatura, edad, genero, ejercicio, objetivo);
+    })
+}
+
+/** Genera el gráfico tipo donut con los gramos de los diferentes macros */
+function graficoDonut(grProteina, grGrasas, grHidratos) {
+    Chart.defaults.global.legend.labels.usePointStyle = true;
+    var config = {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [
+                    Math.round(grProteina),
+                    Math.round(grGrasas),
+                    Math.round(grHidratos),
+                ],
+                backgroundColor: [
+                    'rgb(228, 75, 96)',
+                    'rgb(229, 190, 79)',
+                    'rgb(25, 170, 158)',
+                ],
+                label: 'Dataset 1',
+                borderColor: '#222'
+            }],
+            labels: [
+                'Proteinas',
+                'Grasas',
+                'Hidratos de carbono'
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            animation: {
+                animateScale: true,
+                animateRotate: true
+            },
+            legend: {
+                position: 'bottom',
+                labels: {
+                    fontSize: 20
+                }
+            }
+        }
+    };
+    var ctx = document.getElementById('chart-area').getContext('2d');
+    resizeManager();
+    donut = new Chart(ctx, config);
+}
+
+/** Ecuación de Harris Bennedict */
 function calHarrisBennedict(peso, altura, edad, genero) {
     var salida;
-    if (genero == "mujer") {
+    if (genero === "mujer") {
+        console.log("Calculando Harris-Bennedict para mujer");
         salida = 655.0955 + (9.5634 * peso) + (1.8449 * altura) - (4.6756 * edad);
     } else {
+        console.log("Calculando Harris-Bennedict para hombre");
         salida = 66.4730 + (13.7516 * peso) + (5.0033 * altura) - (6.7550 * edad);
     }
     return salida;
 }
 
+/** Calcula la ingesta recomendada en función del ejercicio fisico */
 function ingestaRecomendada(tmb, ejercicio) {
     var salida;
     switch (ejercicio) {
@@ -88,4 +278,65 @@ function ingestaRecomendada(tmb, ejercicio) {
             break;
     }
     return salida;
+}
+
+/** Detecta en que tipo de pantalla nos encontramos
+ * return: false -> Pantalla horizontal (Monitor)
+ * return: true  -> Pantalla vertical (Teléfono)
+ */
+function detectRatio() {
+    console.log("Ancho:" + $(window).width());
+    console.log("Alto:" + $(window).height());
+    return $(window).width() < $(window).height();
+}
+
+/* Maneja el comportamiento cuando se cambia el tamaño del documento, en este caso solomaneja el comportamiento del gráfico tipo donut */
+function resizeManager() {
+    if (!detectRatio()) {
+        // Pantalla de ordenador
+        if (document.getElementById("chart-area").hasAttribute("height")) {
+            $('#chart-area').removeAttr("height")
+        }
+    } else {
+        // Pantalla de mobil
+        console.log("Ancho de la página: " + $(window).width());
+        $('#chart-area').height($(window).width());
+    }
+}
+
+/** Genera la animación de que los números van ascendiendo hasta
+ * alcanzar el valor objetivo
+ */
+function animateValue(id, end, prefix) {
+    const duration = 1000;
+    const start = 0;
+    // assumes integer values for start and end
+
+    var obj = document.getElementById(id);
+    var range = end - start;
+    // no timer shorter than 50ms (not really visible any way)
+    var minTimer = 50;
+    // calc step time to show all interediate values
+    var stepTime = Math.abs(Math.floor(duration / range));
+
+    // never go below minTimer
+    stepTime = Math.max(stepTime, minTimer);
+
+    // get current time and calculate desired end time
+    var startTime = new Date().getTime();
+    var endTime = startTime + duration;
+    var timer;
+
+    function run() {
+        var now = new Date().getTime();
+        var remaining = Math.max((endTime - now) / duration, 0);
+        var value = Math.round(end - (remaining * range));
+        obj.innerHTML = value + prefix;
+        if (value == end) {
+            clearInterval(timer);
+        }
+    }
+
+    timer = setInterval(run, stepTime);
+    run();
 }
